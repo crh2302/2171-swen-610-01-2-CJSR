85900691cc7600f34d0f5dc5d38b315a75e6aa00
Jeff Palmerino <31670860+jeffrey-palmerino@users.noreply.github.com>
2017-10-10 23:31:51 -0400

    General comments/javadoc updates

--- src/main/java/com/webcheckers/appl/CheckersCenter.java
+++ src/main/java/com/webcheckers/appl/CheckersCenter.java
@@ -3,17 +3,41 @@ package com.webcheckers.appl;
 import com.webcheckers.model.CheckersGame;
 import com.webcheckers.ui.GetGameMenuRoute;
 import spark.Request;
-import spark.Session;;
+import spark.Session;
 import java.util.*;
 
+/**
+ *
+ * The object to coordinate the state of the application.
+ *
+ */
 public class CheckersCenter {
 
-    public List<String> allPlayers;
+    //
+    // attributes
+    //
+
+    public static List<String> allPlayers;
+    public static List<String> inGamePlayers = new ArrayList<>();
+
+    //
+    // constructor
+    //
 
     public CheckersCenter() {
         allPlayers = new ArrayList<>();
     }
 
+
+    /**
+     * Get the {@linkplain CheckersGame game} for the current user
+     *
+     * @param session
+     *   The HTTP session
+     *
+     * @return
+     *   A existing or new {@link CheckersGame}
+     */
     public CheckersGame get(final Session session) {
         // validation
         Objects.requireNonNull(session, "session must not be null");
@@ -26,6 +50,13 @@ public class CheckersCenter {
         return game;
     }
 
+    /**
+     * Ends the user's session, freeing up their name and logging them out
+     *
+     * @param session
+     * @param request
+     *
+     */
     public void end(Session session, Request request) {
         // validation
         Objects.requireNonNull(session, "session must not be null");
@@ -33,14 +64,30 @@ public class CheckersCenter {
         session.removeAttribute("checkGame");
         // do some application-wide book-keeping
         synchronized (this) {  // protect the critical code
-             allPlayers.remove(GetGameMenuRoute.getNameString(request));
+             allPlayers.remove(GetGameMenuRoute.getPlayerNameString(request));
+             inGamePlayers.remove(GetGameMenuRoute.getPlayerNameString(request));
         }
     }
 
-    public boolean isPlayerListEmpty(){
+    /**
+     * to check to see if allPlayers list is empty
+     *
+     * @return
+     *      boolean for if allPlayers list is empty
+     */
+    public boolean isPlayerListEmpty()
+    {
         return allPlayers.isEmpty();
     }
 
+    /**
+     * Checking whether or not the name is valid
+     *
+     * @param playerName
+     *
+     * @return
+     *      boolean to determine whether or not the allPlayers list contains the playerName
+     */
     public boolean add(String playerName)
     {
         if (allPlayers.contains(playerName))
@@ -53,6 +100,11 @@ public class CheckersCenter {
         }
     }
 
+    /**
+     *
+     * @return
+     *      allPlayer's list
+     */
     public List<String> getAllPlayers() {
         return allPlayers;
     }
