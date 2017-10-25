bcfcae2aa74e182af8457338fa00ce407d7c9a70
Jeff Palmerino <31670860+jeffrey-palmerino@users.noreply.github.com>
2017-10-25 13:16:41 -0400

    Add files via upload

--- src/test/java/com/webcheckers/model/PieceTest.java
+++ src/test/java/com/webcheckers/model/PieceTest.java
@@ -10,24 +10,11 @@ public class PieceTest {
     Piece piece;
     @Before
     public void setUp() throws Exception {
-        piece = new Piece("Red");
+      piece = new Piece("Red");
     }
 
     @After
     public void tearDown() throws Exception {
-    }
-
-    @Test
-    public void setType() throws Exception {
-        piece.setType("KING");
-        assertEquals("KING",piece.getType());
-
-    }
-
-    @Test
-    public void setColor() throws Exception {
-        piece.setColor("Black");
-        assertEquals("Black",piece.getColor());
     }
 
     @Test
