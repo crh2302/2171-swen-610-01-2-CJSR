a9e70cf01c96bc87d293595258508d4a45597d6f
Cervantes Hernandez <31755791+crh2302@users.noreply.github.com>
2017-10-25 10:17:26 -0400

    Tests done by Raseshwari
    After the revert merge the files went away

---
+++ src/test/java/GetGameRouteTest.java
@@ -0,0 +1,77 @@
+package com.webcheckers.ui;
+
+import com.webcheckers.appl.CheckersCenter;
+import com.webcheckers.model.Board;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import spark.Request;
+import spark.Response;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.mockingDetails;
+import static org.mockito.Mockito.when;
+
+public class GetGameRouteTest {
+    CheckersCenter checkersCenter;
+    Request request;
+    Response response;
+    ArrayList inGamePlayers;
+    Board board;
+
+
+    @Before
+    public void setUp() throws Exception {
+        checkersCenter=mock(CheckersCenter.class);
+        request=mock(Request.class);
+        response=mock(Response.class);
+        inGamePlayers=new ArrayList();
+        inGamePlayers.add("Rucha");
+        inGamePlayers.add("Shinchan");
+        board=mock(Board.class);
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void handle() throws Exception {
+
+
+
+        assertNotNull(checkersCenter); //assert checkerCenter is not null
+        assertNotNull(response); //assert response is not null
+        assertNotNull(request); //assert request is not null
+
+        //player is not playing a game assertFalse
+        when(request.queryParams("playerName")).thenReturn("Ross");
+        when(checkersCenter.getInGamePlayers()).thenReturn(inGamePlayers);
+        assertFalse(checkersCenter.getInGamePlayers().contains(request.queryParams("playerName")));
+
+        //when opponent is not playing a game assertFalse
+        when(request.queryParams("opponent")).thenReturn("Kirr");
+        when(checkersCenter.getInGamePlayers()).thenReturn(inGamePlayers);
+        assertFalse(checkersCenter.getInGamePlayers().contains(request.queryParams("opponent")));
+
+        //when player with same name is already in a game assertTrue
+        when(request.queryParams("playerName")).thenReturn("Shinchan");
+        when(checkersCenter.getInGamePlayers()).thenReturn(inGamePlayers);
+        assertTrue(checkersCenter.getInGamePlayers().contains(request.queryParams("playerName")));
+
+        //when opponent is already playing a game assertTrue
+        when(request.queryParams("opponent")).thenReturn("Rucha");
+        when(checkersCenter.getInGamePlayers()).thenReturn(inGamePlayers);
+        assertTrue(checkersCenter.getInGamePlayers().contains(request.queryParams("opponent")));
+
+
+
+        assertNotNull(board);
+
+    }
+
+}
---
+++ src/test/java/PieceTest.java
@@ -0,0 +1,37 @@
+package com.webcheckers.model;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PieceTest {
+    Piece piece;
+    @Before
+    public void setUp() throws Exception {
+
+      piece = new Piece("Red");
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void getType() throws Exception {
+        assertEquals("SINGLE",piece.getType());
+    }
+
+    @Test
+    public void getColor() throws Exception {
+        assertEquals("Red",piece.getColor());
+    }
+
+    @Test
+    public void crownPiece() throws Exception {
+        piece.crownPiece();
+        assertEquals("KING",piece.getType());
+    }
+
+}
