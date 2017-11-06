c6b1035fa93ae3c0cecd373c9ccee98ba92b7141
Jeff Palmerino <31670860+jeffrey-palmerino@users.noreply.github.com>
2017-11-06 14:53:42 -0500

    Add files via upload

--- src/test/java/com/webcheckers/model/CheckersGameTest.java
+++ src/test/java/com/webcheckers/model/CheckersGameTest.java
@@ -3,42 +3,85 @@ package com.webcheckers.model;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Spy;
+
+import java.util.LinkedList;
+
+import static org.mockito.Mockito.mock;
 
 import static org.junit.Assert.*;
 
 public class CheckersGameTest
 {
-    private CheckersGame checkersGame;
-    final private String COLOR_RED = "RED";
-    final private String COLOR_WHITE = "WHITE";
+    private CheckersGame CuT;
+    private String playerMock;
+    private String opponentMock;
+    private Move moveMock;
+
+    @Spy
+    LinkedList<Move> spyMoves;
 
     @Before
     public void setUp() throws Exception
     {
-        checkersGame = new CheckersGame("hey", "cool");
-        checkersGame.setTurnColor(COLOR_RED);
+        this.playerMock = "Bill";
+        this.opponentMock = "Steve";
+        spyMoves = new LinkedList<>();
+        moveMock = mock(Move.class);
+        CuT = new CheckersGame(playerMock,opponentMock);
     }
 
     @After
     public void tearDown() throws Exception
     {
+
+    }
+
+
+    @Test
+    public void test_getSetBoard() {
+        Board board1 = new Board();
+        Board board2 = new Board();
+
+        CuT.setBoard(board1);
+        assertSame(board1,CuT.getBoard());
+        CuT.setBoard(board2);
+        assertSame(board2,CuT.getBoard());
+
     }
 
     @Test
-    public void getTurnColor() throws Exception
-    {
-        assertEquals(COLOR_RED,checkersGame.getTurnColor());
+    public void test_getPlayer(){
+        assertEquals(this.playerMock,CuT.getPlayer());
+
     }
 
     @Test
-    public void setTurnColor() throws Exception
+    public void test_getOpponent(){
+        assertEquals(this.opponentMock,CuT.getOpponent());
+    }
+
+    @Test
+    public void test_hasPlayer()
     {
-        checkersGame.setTurnColor(COLOR_WHITE);
-        assertEquals(COLOR_WHITE,checkersGame.getTurnColor());
+        assertTrue(CuT.hasPlayer(playerMock));
+        assertTrue(CuT.hasPlayer(opponentMock));
+        assertFalse(CuT.hasPlayer(""));
+        assertFalse(CuT.hasPlayer("AnotherName"));
 
-        checkersGame.setTurnColor(COLOR_RED);
-        assertEquals(COLOR_RED,checkersGame.getTurnColor());
+    }
 
+    @Test
+    public void test_isTurnAnToggleTurn()
+    {
+        assertTrue(CuT.isPlayerTurn(playerMock));
+        assertFalse(CuT.isPlayerTurn(opponentMock));
+        CuT.switchTurn();
+        assertFalse(CuT.isPlayerTurn(playerMock));
+        assertTrue(CuT.isPlayerTurn(opponentMock));
+        CuT.switchTurn();
+        assertTrue(CuT.isPlayerTurn(playerMock));
+        assertFalse(CuT.isPlayerTurn(opponentMock));
     }
 
 }
