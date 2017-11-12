e69e089950c2de01d3cf237159ded5e4da7a9e68
crh2302 <crh2302@rit.edu>
2017-11-12 16:30:51 -0500

    Removed unused code from WebServerTest

--- src/test/java/com/webcheckers/ui/WebServerTest.java
+++ src/test/java/com/webcheckers/ui/WebServerTest.java
@@ -2,29 +2,20 @@ package com.webcheckers.ui;
 import com.webcheckers.appl.CheckersCenter;
 import org.junit.Before;
 import org.junit.Test;
-import spark.Request;
-import spark.Response;
 import spark.TemplateEngine;
 
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.mock;
 
 public class WebServerTest {
-
-
     TemplateEngine templateEngineMock;
     CheckersCenter checkersCenterMock;
-
     WebServer CuT;
-    Request request;
-    Response response;
 
     @Before
     public void setUp() throws Exception {
         checkersCenterMock = mock(CheckersCenter.class);
         templateEngineMock = mock(TemplateEngine.class);
-        request = mock(Request.class);
-        response = mock(Response.class);
         CuT = new WebServer(checkersCenterMock, templateEngineMock);
     }
 
